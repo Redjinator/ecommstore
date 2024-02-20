5574ce54de4c64dbc3a996f4f0739dc3b7c89e28
Redjinator <rmcpherson@northislandcollege.ca>
2024-02-19 19:29:30 -0800

    fix: font-awesome

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -15,6 +15,8 @@ module.exports = {
     },
   },
   plugins: [
+    require('@tailwindcss'),
+    require ('@autoprefixer'),
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
